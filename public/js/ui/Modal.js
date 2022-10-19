827d46a838f494398f5ec0d8ec74e7742ac35d69
Наталья <lu_na87@mail.ru>
2022-10-19 21:51:26 +0300

    Кнопка управления боковой колонкой, управление окнами

--- public/js/ui/Modal.js
+++ public/js/ui/Modal.js
@@ -12,6 +12,11 @@ class Modal {
    * необходимо выкинуть ошибку.
    * */
   constructor(element){
+    if(!element) {
+      throw new Error("Передан пустой элемент в класс Modal")
+    }
+    this.element = element;
+    this.registerEvents();
 
   }
 
@@ -21,7 +26,9 @@ class Modal {
    * (с помощью метода Modal.onClose)
    * */
   registerEvents() {
-
+    this.element.querySelectorAll('[data-dismiss="modal"]').forEach(bn => {
+      bn.onclick = e => this.onClose(e);
+    });
   }
 
   /**
@@ -29,19 +36,20 @@ class Modal {
    * Закрывает текущее окно (Modal.close())
    * */
   onClose(e) {
-
+    e.preventDefault();
+    this.close();
   }
   /**
    * Открывает окно: устанавливает CSS-свойство display
    * со значением «block»
    * */
-  open() {
-
+   open() {
+    this.element.style.display = 'block';
   }
   /**
    * Закрывает окно: удаляет CSS-свойство display
    * */
   close(){
-
+    this.element.style.removeProperty('display');
   }
 }
